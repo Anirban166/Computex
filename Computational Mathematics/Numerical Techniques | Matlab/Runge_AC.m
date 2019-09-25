a02ecfb234f496248f8cea78d37cab482f66fba2
Anirban Chetia <bloodraven166@gmail.com>
2019-09-25 22:19:34 +0530

    2nd order Runge for (u''+(7t^2)u'-(6e^(-t))u)=t-3.

---
+++ Computational Mathematics/Numerical Techniques | Matlab/Runge_AC.m
@@ -0,0 +1,16 @@
+% Done By : Anirban Chetia
+function[dwdt]=odefile_AC(t,w)
+f1=w(2);
+f2=7*t^2*w(2)-6*sqrt(t^2+1)*w(1)+t^2-2*t+3;
+dwdt=[f1;f2];
+
+% plot commands in command window :
+ic=[-2,1];
+range=[0 10];
+[t,w]=ode45(@odefile_AC,range,ic);
+plot(t,w(:,1),'-r');
+hold on
+plot(t,w(:,2),'-b');
+axis([0 1 -3 6])
+% or
+axis([0 1 -1 3])
