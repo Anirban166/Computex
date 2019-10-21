537aec0a8e2697773efe8d956f61cc18082c2dfc
Anirban Chetia <bloodraven166@gmail.com>
2019-10-21 19:17:50 +0530

    Update and rename Simpson13.m to Simpson38.m

--- Numerical Techniques | Matlab/Simpson38.m
+++ Numerical Techniques | Matlab/Simpson38.m
@@ -8,5 +8,6 @@ end
 h=(b-a)/2;
 x=a+[O:n]*h;
 fval=f(x);
-fval(find(fval==inf))=real max;
+fval(find(fval==inf))=realmax;
+fval(find(fval==-inf))=-realmax;
 area=3*h/8*(fval(1)+fval(n+1)+3*sum(fval(2:n))-sum(fval(4:3:n-2)));
