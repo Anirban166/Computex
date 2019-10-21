d6ea98b912071e78ebf3247fb62f3889aa8d7d05
Anirban Chetia <bloodraven166@gmail.com>
2019-10-21 19:17:03 +0530

    Create Simpson13.m

---
+++ Numerical Techniques | Matlab/Simpson13.m
@@ -0,0 +1,12 @@
+function[Area]=S38rule(f,a,b,n)
+if mod(n,3)==1
+ n=n+2;
+end
+if mod(n,3)==2
+ n=n+1;
+end
+h=(b-a)/2;
+x=a+[O:n]*h;
+fval=f(x);
+fval(find(fval==inf))=real max;
+area=3*h/8*(fval(1)+fval(n+1)+3*sum(fval(2:n))-sum(fval(4:3:n-2)));
