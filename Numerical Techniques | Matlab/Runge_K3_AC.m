db29d296f7b5a76d7d17f6d9c40fcaf8105069c0
Anirban Chetia <bloodraven166@gmail.com>
2019-10-26 12:41:01 +0530

    3rd order Runge for u'''-(7e^(-6t))u''-(e^(-t^2))u'+(6/(t^2))u=0.

--- Numerical Techniques | Matlab/Runge_K3_AC.m
+++ Numerical Techniques | Matlab/Runge_K3_AC.m
@@ -1,18 +1,18 @@
 % Done By : Anirban Chetia
 function[dwdt]=odefile_AC(t,w)
 f1=w(2);
-f2=7*t^2*w(2)-6*sqrt(t^2+1)*w(1)+t^2-2*t+3;
-dwdt=[f1;f2];
+f2=w(3);
+f3=(-7*e^(-6*t))*w(3)-(e^(-t^2))*w(2)+(6/(t^2))*w(1);
+dwdt=[f1;f2;f3];
 
-% plot commands in command window :
+% plot commands in command window below :
 ic=[-2,1];
 range=[0 10];
 [t,w]=ode45(@odefile_AC,range,ic);
 plot(t,w(:,1),'-r');
 hold on
 plot(t,w(:,2),'-b');
-axis([0 1 -3 6])
-% or
-axis([0 1 -1 3])
+plot(t,w(:,3),'-gr');
 
 % 3rd order Runge for u'''-(7e^(-6t))u''-(e^(-t^2))u'+(6/(t^2))u=0.
+ 
